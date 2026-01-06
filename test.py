10955378d4ae9deafc8b193d3e23991cc5201230
clorb-3 <claude.freeman.evert@gmail.com>
2026-01-06 14:40:19 -0500

    the class work for today

---
+++ test.py
@@ -0,0 +1,6 @@
+book = input("what did you read over the break?\n:>")
+place = input("where did you go over the break?\n:>")
+game = input("what did you play over the break?\n:>")
+gift = input("what was your favorite gift you receved?\n:>")
+print("you read " + book + " you went to " + place +
+      " you played " + game + " and you got " + gift)
