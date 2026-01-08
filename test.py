d5a6c8d4c19f42ff5cf9df7d357e397897411a40
clorb-3 <claude.freeman.evert@gmail.com>
2026-01-08 09:44:05 -0500

    program yay

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
-book = input("what did you read over the break?\n:>")
-place = input("where did you go over the break?\n:>")
-game = input("what did you play over the break?\n:>")
-gift = input("what was your favorite gift you receved?\n:>")
-print("you read " + book + " you went to " + place +
-      " you played " + game + " and you got " + gift)
+itrationCount = int(input('count to: ')) + 2
+numCount = 1
+
+
+def iterate():
+    global numCount
+    global isOdd
+    global isEven
+    isOdd = str(numCount) + ' is odd\n'
+    isEven = str(numCount + 2) + ' is even'
+    print(isOdd + isEven)
+    numCount = numCount + 2
+
+
+while numCount < itrationCount:
+    iterate()
